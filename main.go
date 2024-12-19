7f7a4f30441ae834ce81f4360f1b4ba8801a0ca2
Abhi5453Abhi <abhi5453abhi@gmail.com>
2024-12-20 00:46:09 +0530

    Auto-commit: Fri, 20 Dec 2024 00:46:09 IST

--- main.go
+++ main.go
@@ -11,12 +11,19 @@ func PrintPing(ping chan bool, pong chan bool, wg *sync.WaitGroup) {
 	for i := 0; i <= 10; i++ {
 		<-ping
 		fmt.Println("Ping")
+		pong <- true
 	}
 
 }
 
 func PrintPong(ping chan bool, pong chan bool, wg *sync.WaitGroup) {
 	defer wg.Done()
+
+	for i := 0; i <= 10; i++ {
+		<-pong
+		fmt.Println("Ping")
+		ping <- true
+	}
 }
 
 func main() {
