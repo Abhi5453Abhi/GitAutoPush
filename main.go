b75b961a08c3a3ac106336dd2b2c805ea13f6f47
Abhi5453Abhi <abhi5453abhi@gmail.com>
2024-12-19 19:12:17 +0530

    Auto-commit: Thu, 19 Dec 2024 19:12:17 IST

--- main.go
+++ main.go
@@ -8,21 +8,19 @@ import (
 func PrintEven(even chan bool, odd chan bool, wg *sync.WaitGroup) {
 	defer wg.Done()
 	for i := 0; i <= 10; i += 2 {
-		<-even
+		<-even // Wait for the signal to print an even number
 		fmt.Println(i)
-		odd <- true
+		odd <- true // Signal the odd goroutine
 	}
-	// close(even)
 }
 
 func PrintOdd(even chan bool, odd chan bool, wg *sync.WaitGroup) {
 	defer wg.Done()
-	for i := 1; i <= 10; i += 2 {
-		<-odd
+	for i := 1; i <= 9; i += 2 {
+		<-odd // Wait for the signal to print an odd number
 		fmt.Println(i)
-		even <- true
+		even <- true // Signal the even goroutine
 	}
-	// close(odd)
 }
 
 func main() {
@@ -35,6 +33,6 @@ func main() {
 	go PrintEven(even, odd, &wg)
 	go PrintOdd(even, odd, &wg)
 
-	even <- true
-	wg.Wait()
+	even <- true // Start the process by signaling the even goroutine
+	wg.Wait()    // Wait for both goroutines to complete
 }
