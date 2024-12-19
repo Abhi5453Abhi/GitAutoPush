d3d10bb37ae4dcf121c5c48a2e2f519926e1836d
Abhi5453Abhi <abhi5453abhi@gmail.com>
2024-12-19 14:46:34 +0530

    Auto-commit: Thu, 19 Dec 2024 14:46:34 IST

--- main.go
+++ main.go
@@ -19,12 +19,16 @@ func main() {
 	str := "abcabcc"
 	ans := min(1, len(str))
 	ma := make(map[string]int)
+	//a -> 1
 
 	i := 0
 	j := 0
 
 	for j < len(str) {
-		ma[sstr[i]]++
+		ma[string(str[i])]++
+		for {
+			if(ma[s])
+		}
 	}
 
 	fmt.Println(ans)
