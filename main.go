639e3058de6d4f360366de54b1d9dc78ba091487
Abhi5453Abhi <abhi5453abhi@gmail.com>
2024-12-19 19:16:28 +0530

    Auto-commit: Thu, 19 Dec 2024 19:16:28 IST

--- main.go
+++ main.go
@@ -2,42 +2,35 @@ package main
 
 import (
 	"fmt"
-	"sync"
 )
 
-func PrintEven(even chan bool, odd chan bool, wg *sync.WaitGroup, done chan bool) {
-	// defer wg.Done()
-	// defer close(done)
-	for i := 0; i <= 10; i += 2 {
-		<-even // Wait for the signal to print an even number
-		fmt.Println(i)
-		odd <- true // Signal the odd goroutine
-	}
-	done <- true
-}
-
-func PrintOdd(even chan bool, odd chan bool, wg *sync.WaitGroup, done chan bool) {
-	// defer wg.Done()
-	for i := 1; i <= 9; i += 2 {
-		<-odd // Wait for the signal to print an odd number
-		fmt.Println(i)
-		even <- true // Signal the even goroutine
-	}
-	close(even)
-}
-
 func main() {
-	wg := sync.WaitGroup{}
-
+	// Create channels for communication
 	even := make(chan bool)
 	odd := make(chan bool)
-	done := make(chan bool)
 
-	// wg.Add(2)
-	go PrintEven(even, odd, &wg, done)
-	go PrintOdd(even, odd, &wg, done)
+	// Launch a goroutine to print even numbers
+	go func() {
+		for i := 0; i <= 10; i += 2 {
+			<-even // Wait for signal from the main routine
+			fmt.Print(i)
+			odd <- true // Signal the odd goroutine
+		}
+	}()
 
-	even <- true // Start the process by signaling the even goroutine
-	// wg.Wait()    // Wait for both goroutines to complete
-	<-done
+	// Launch a goroutine to print odd numbers
+	go func() {
+		for i := 1; i <= 10; i += 2 {
+			<-odd // Wait for signal from the even goroutine
+			fmt.Print(i)
+			even <- true // Signal the even goroutine
+		}
+	}()
+
+	// Start the printing process
+	even <- true // Trigger the even goroutine
+	<-odd        // Wait for the last odd number to complete
+
+	// End of main function
+	fmt.Println()
 }
