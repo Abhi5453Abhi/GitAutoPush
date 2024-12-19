0d87e5c042e1899932ce4f495f1dbf7480b91de3
Abhi5453Abhi <abhi5453abhi@gmail.com>
2024-12-19 19:24:45 +0530

    Auto-commit: Thu, 19 Dec 2024 19:24:45 IST

--- main.go
+++ main.go
@@ -1,15 +1,22 @@
 package main
 
+import "fmt"
+
 func main() {
 	even := make(chan bool)
 	odd := make(chan bool)
+	evenInt := make(chan int)
+	oddInt := make(chan int)
 
 	go PrintEven()
 	go PrintOdd()
 
 	for i := 0; i <= 10; i++ {
-		select{
-			cas
+		select {
+		case <-even:
+			fmt.Println(evenInt)
+		case <-odd:
+			fmt.Println(oddInt)
 		}
 	}
 }
