4d8d68e45e45318cde2ff5c72a65e596e12a9e9f
Abhi5453Abhi <abhi5453abhi@gmail.com>
2024-12-19 14:52:44 +0530

    Auto-commit: Thu, 19 Dec 2024 14:52:44 IST

--- main.go
+++ main.go
@@ -25,10 +25,10 @@ func main() {
 	j := 1
 
 	for j < len(str) {
-		for {
-			if ma[string(str[j])] >= 1 {
-				break
-			}
+		for ma[string(str[j])] >= 1 {
+			// if ma[string(str[j])] >= 1 {
+			// 	break
+			// }
 			ma[string(str[i])] = 0
 			i++
 		}
