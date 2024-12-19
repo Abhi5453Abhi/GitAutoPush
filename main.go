73fc1fb69d64b080462ecb340d011d2d508e2bdb
Abhi5453Abhi <abhi5453abhi@gmail.com>
2024-12-19 13:49:10 +0530

    Auto-commit: Thu, 19 Dec 2024 13:49:10 IST

--- main.go
+++ main.go
@@ -5,6 +5,24 @@ import (
 	"sync"
 )
 
+func PrintPing(ping chan bool, pong chan bool, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for i := 0; i < 10; i++ {
+		<-ping
+		fmt.Println("Ping")
+		pong <- true
+	}
+}
+
+func PrintPong(ping chan bool, pong chan bool, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for i := 0; i < 10; i++ {
+		<-ping
+		fmt.Println("Ping")
+		pong <- true
+	}
+}
+
 func main() {
 	wg := sync.WaitGroup{}
 
@@ -12,13 +30,10 @@ func main() {
 	pong := make(chan bool, 10)
 
 	wg.Add(2)
+	go PrintPing()
+	go PrintPong()
 	go func() {
-		defer wg.Done()
-		for i := 0; i < 10; i++ {
-			<-ping
-			fmt.Println("Ping")
-			pong <- true
-		}
+
 	}()
 	go func() {
 		defer wg.Done()
