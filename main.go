76e39c186d452ebb74d6631eed26ddf3a18b4db1
Abhi5453Abhi <abhi5453abhi@gmail.com>
2024-12-20 00:43:34 +0530

    Auto-commit: Fri, 20 Dec 2024 00:43:34 IST

--- main.go
+++ main.go
@@ -1,44 +1,8 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-)
-
-func PrintPing(ping chan bool, pong chan bool, wg *sync.WaitGroup) {
-	defer wg.Done()
-	for i := 0; i <= 10; i += 2 {
-		select {
-		case <-ping:
-			fmt.Println("ping")
-			pong <- true
-		}
-	}
-}
-
-func PrintPong(ping chan bool, pong chan bool, wg *sync.WaitGroup) {
-	defer wg.Done()
-	for i := 0; i <= 10; i += 2 {
-		select {
-		case <-pong:
-			fmt.Println("pong")
-			ping <- true
-		default:
-			fmt.Println("Ping CLOSSED")
-			close(ping)
-		}
-	}
-}
-
 func main() {
 	ping := make(chan bool)
 	pong := make(chan bool)
 
-	wg := sync.WaitGroup{}
-
-	wg.Add(2)
-	go PrintPing(ping, pong, &wg)
-	go PrintPong(ping, pong, &wg)
-	ping <- true
-	wg.Wait()
+	wg := s
 }
