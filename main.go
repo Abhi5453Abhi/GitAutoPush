a86d92d8f4113429c4f0c8527f58a57e6217cc0f
Abhi5453Abhi <abhi5453abhi@gmail.com>
2024-12-20 01:20:29 +0530

    Auto-commit: Fri, 20 Dec 2024 01:20:29 IST

--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
+import "fmt"
+
 func getNumbers(ch chan int) {
 	for i := 1; i <= 10; i++ {
 		ch <- i
 	}
-	clos
+	close(ch)
+}
+
+func PrintSquares(ch chan int) {
+	for i := range ch {
+		fmt.Println(i * i)
+	}
 }
 
 func main() {
+	wg := s
 	ch := make(chan int)
 
 	go getNumbers(ch)
