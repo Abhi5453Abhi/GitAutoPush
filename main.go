ea465d02dc92698fdfc7ea5bbb6e9fefc1f83fed
Abhi5453Abhi <abhi5453abhi@gmail.com>
2024-12-19 14:10:40 +0530

    Auto-commit: Thu, 19 Dec 2024 14:10:40 IST

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import "sync"
+
 func main() {
-	wg := s
+	wg := sync.WaitGroup{}
+
+	even := make(chan bool)
+	odd := make(chan bool)
+
+	wg.Add(2)
+	go PrintEven(even, odd, &wg)
+	go PrintOdd(even, odd, &wg)
+
+	even <- true
+	wg.Wait()
 }
