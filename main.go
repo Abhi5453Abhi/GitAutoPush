d604b4da7fb2916c59d5fb1ff705ac69395fb27e
Abhi5453Abhi <abhi5453abhi@gmail.com>
2024-12-20 01:10:10 +0530

    Auto-commit: Fri, 20 Dec 2024 01:10:10 IST

--- main.go
+++ main.go
@@ -1,15 +1,20 @@
 package main
 
 type ParsedData struct {
-	name s
+	name   string
+	age    int
+	skills []string
 }
 
 func main() {
+	var pars
 	data := `{
 		"name": "Alice",
 		"age": 30,
 		"skills": ["Golang", "Docker", "Kubernetes"]
 	  }
 	  `
+
+	parsedData
 
 }
