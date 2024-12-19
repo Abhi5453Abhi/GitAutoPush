88672421c583fdda29ce8827a86f5d7edfe73edb
Abhi5453Abhi <abhi5453abhi@gmail.com>
2024-12-19 13:47:05 +0530

    Auto-commit: Thu, 19 Dec 2024 13:47:05 IST

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+)
+
 func main() {
-	wg := s
+	wg := sync.WaitGroup{}
+
+	ping := make(chan bool)
+	pong := make(chan bool)
+
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 10; i++ {
+			<-ping
+			fmt.Println("Ping")
+			pong <- true
+		}
+	}()
 }
