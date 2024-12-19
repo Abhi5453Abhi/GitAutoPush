aa38c47d57695bc580ad0f95b3a4cc6b743068fc
Abhi5453Abhi <abhi5453abhi@gmail.com>
2024-12-19 13:14:28 +0530

    Auto-commit: Thu, 19 Dec 2024 13:14:28 IST

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	interval := time.Second * 10 // Set the interval to 10 minutes
+	interval := time.Second * 20 // Set the interval to 10 minutes
 
 	for {
 		fmt.Println("Starting auto-push process...")
