cd130a3523b7efa230ff5b226245600dd26e06b0
Abhi5453Abhi <abhi5453abhi@gmail.com>
2024-12-20 12:34:13 +0530

    Auto-commit: Fri, 20 Dec 2024 12:34:13 IST

--- main.go
+++ main.go
@@ -1,33 +1,12 @@
+/**
+
+Worker pool pattern
+
+**/
+
 package main
 
-import (
-	"context"
-	"fmt"
-	"time"
-)
-
 func main() {
-	ctx := context.Background()
-
-	thirdPartyApi(ctx)
-}
-
-func thirdPartyApi(ctx context.Context) {
-	ctxWithTo, cancel := context.WithTimeout(ctx, 4*time.Second)
-	defer cancel()
-
-	done := make(chan bool)
-
-	go func() {
-		time.Sleep(3 * time.Second)
-		done <- true
-	}()
-
-	select {
-	case <-done:
-		fmt.Println("Work is done")
-	case <-ctxWithTo.Done():
-		fmt.Println("Deadline exceeded")
-	}
-
+	ch := make(chan int)
+	wg := s
 }
