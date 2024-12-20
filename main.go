94b12e75a34c838e9d1505ea1afa0d1f19e24791
Abhi5453Abhi <abhi5453abhi@gmail.com>
2024-12-20 12:35:18 +0530

    Auto-commit: Fri, 20 Dec 2024 12:35:18 IST

--- main.go
+++ main.go
@@ -6,7 +6,16 @@ Worker pool pattern
 
 package main
 
+import "sync"
+
+func GetSquare(val int, wg *s)
+
 func main() {
-	ch := make(chan int)
-	wg := s
+	ch := make(chan int, 1000)
+	wg := sync.WaitGroup{}
+
+	for i := 0; i <= 10000; i++ {
+		wg.Add(1)
+		go GetSquare(i)
+	}
 }
