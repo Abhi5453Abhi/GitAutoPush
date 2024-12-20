7ab6cdcd3c7bee66bae0cb4a4a9482578412060b
Abhi5453Abhi <abhi5453abhi@gmail.com>
2024-12-20 13:05:15 +0530

    Auto-commit: Fri, 20 Dec 2024 13:05:15 IST

--- main.go
+++ main.go
@@ -32,8 +32,8 @@ func GetResult(val int, result chan int) {
 func main() {
 	start := time.Now()
 	defer fmt.Println(time.Since(start))
-	ch := make(chan int, 100)
-	res := make(chan int, 100)
+	ch := make(chan int)
+	res := make(chan int)
 	wg := sync.WaitGroup{}
 
 	go func() {
@@ -43,7 +43,7 @@ func main() {
 		close(ch)
 	}()
 
-	for i := 0; i <= 5; i++ {
+	for i := 0; i <= 2; i++ {
 		wg.Add(1)
 		go WorkerFunction(ch, &wg, res)
 	}
